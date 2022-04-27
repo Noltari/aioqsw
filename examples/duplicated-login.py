c77e8f85bcdc163defdf7ba80906e235741d2cba
Álvaro Fernández Rojas <noltari@gmail.com>
2022-04-27 19:09:42 +0200

    Improve examples
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- examples/duplicated-login.py
+++ examples/duplicated-login.py
@@ -1,4 +1,4 @@
-"""Basic QNAP QSW client example."""
+"""QNAP QSW duplicated login example."""
 import asyncio
 import json
 
@@ -10,21 +10,22 @@ from aioqsw.localapi import QnapQswApi
 
 
 async def main():
-    """Basic QNAP QSW client example."""
+    """QNAP QSW duplicated login example."""
 
     async with aiohttp.ClientSession() as aiohttp_session:
-        client = QnapQswApi(aiohttp_session, _config.QNAP_QSW_OPTIONS)
+        qsw = QnapQswApi(aiohttp_session, _config.QNAP_QSW_OPTIONS)
         try:
-            system_board = await client.validate()
+            system_board = await qsw.validate()
             if system_board is not None:
                 print(f"System Board: {system_board.data()}")
-            await client.update()
-            print(json.dumps(client.data(), indent=4, sort_keys=True))
+
+            await qsw.update()
+            print(json.dumps(qsw.data(), indent=4, sort_keys=True))
 
             await asyncio.sleep(20)
 
-            await client.update()
-            print(json.dumps(client.data(), indent=4, sort_keys=True))
+            await qsw.update()
+            print(json.dumps(qsw.data(), indent=4, sort_keys=True))
         except APIError:
             print("Invalid host.")
 
